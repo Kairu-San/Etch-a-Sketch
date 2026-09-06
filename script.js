e506da3ba622245f97234c1fb32c15715bf8df23
KairuSan <kylelegaspi10@gmail.com>
2026-09-06 23:32:02 +0800

    feat: add grid and other base files

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('#container');
+
+for (i = 0; i < 16; i++) {
+    for (j = 0; j < 16; j++) {
+        const grid = document.createElement('div');
+        grid.setAttribute("class", `square r${i+1} c${j+1}`);
+        container.appendChild(grid); 
+    }
+}
