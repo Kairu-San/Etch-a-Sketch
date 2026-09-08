1ebae9f487d86ff34172f39c8adffa2c3f6fea06
M306 User <M306User@lab.uc-bcf.local>
2026-09-08 08:33:14 +0800

    feat: add trail functionality

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ for (i = 0; i < 16; i++) {
     for (j = 0; j < 16; j++) {
         const grid = document.createElement('div');
         grid.setAttribute("class", `square r${i+1} c${j+1}`);
+
+        grid.addEventListener('mouseenter', ()=>{
+            grid.classList.add('hovered');
+        });
+
         container.appendChild(grid); 
     }
 }
+
+
